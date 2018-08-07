a0ba600fadb1c7ef78be5f278ab36531ab100912
TuanDV <dangtuannoone@gmail.com>
2018-08-07 09:34:39 +0700

    Update a rewriting question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,7 +2,7 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   scope :true_answer,
-    ->(question_id){where(question_id: question_id, truth:1)}
+    ->(question_id){where(question_id: question_id, truth: 1)}
   validates :content, presence: true,
     length: {maximum: Settings.validate.answer.content.maxlength}
 end
