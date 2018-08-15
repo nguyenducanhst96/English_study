e71ce0b953e3390a7d4e83ca7940a2790f89896f
anhnd <ducanhst962@gmail.com>
2018-08-15 09:01:23 +0700

    Init UI for user

---
+++ app/assets/javascripts/user_application.js
@@ -0,0 +1,5 @@
+//= require user/jquery-3.2.1.min
+//= require user/bootstrap4/popper
+//= require user/bootstrap4/bootstrap.min
+//= require user/plugins/OwlCarousel2-2.2.1/owl.carousel
+//= require user/plugins/easing/easing
